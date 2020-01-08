78d39f06af67db4573171bccb66d5f66b538f2e8
hridyansh68 <hridyansh68@gmai.com>
2020-01-08 15:56:19 +0530

    Springboot APIs dealing with JPA and two tables

---
+++ spring-api/src/main/java/com/java/springbootquickstart/springapi/course/CourseService.java
@@ -0,0 +1,47 @@
+package com.java.springbootquickstart.springapi.course;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseService {
+	
+	    @Autowired
+	    private CourseRepository courseRepository;
+	
+
+        public List<Course> getAllCourses(String topicId){
+        	List<Course> courses = new ArrayList<>();
+        	courseRepository.findByTopicId(topicId)
+        	.forEach(courses::add);
+        	return courses;
+        }
+        
+        public Optional<Course> getCourse(String id) {
+        	return courseRepository.findById(id);
+        }
+
+		public void addCourse(Course topic) {
+			courseRepository.save(topic);
+			 
+		}
+
+		public void updateCourse(Course course ) {
+			// TODO Auto-generated method stub
+			
+			courseRepository.save(course);
+			
+		}
+
+
+		public void deleteCourse(String id) {
+			// TODO Auto-generated method stub
+			courseRepository.deleteById(id);
+			
+		}
+}
