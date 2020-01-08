1e6dc33b5948569f105e4b7c5b74f8470cb6a0cc
hridyansh68 <hridyansh68@gmai.com>
2020-01-08 17:25:57 +0530

    using mongodb as data for rest

---
+++ accessing-mongodb-data-rest/src/test/java/com/example/sde101/accessingmongodbdatarest/AccessingMongodbDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sde101.accessingmongodbdatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingMongodbDataRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
